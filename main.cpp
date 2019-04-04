3bc4a5fce39e8776cadec7167568046441bcb277
Riki Singh Khorana <rkhorana@andrew.cmu.edu>
2019-04-03 23:16:32 -0400

    Update progress

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <algorithm>
 #include <ctime>
+#include <cmath>
 
 using namespace cv;
 using namespace std;
@@ -12,28 +13,24 @@ using namespace std;
 // Seam carving helper functions //
 //-------------------------------//
 
-void energy_function(Mat &image, Mat &output) {
+/**
+ * Maps an input {frame} in gray scale to an {output} via an energy map.
+ * Directly sourced from {@link https://github.com/loc-trinh/seamCarving}.
+ *
+ * @param frame - The gray scale frame.
+ * @param output - The energy map of the frame.
+ */
+void energy_function(Mat &frame, Mat &output) {
     Mat dx, dy;
-    Sobel(image, dx, CV_64F, 1, 0);
-    Sobel(image, dy, CV_64F, 0, 1);
+    Sobel(frame, dx, CV_64F, 1, 0);
+    Sobel(frame, dy, CV_64F, 0, 1);
     magnitude(dx, dy, output);
 
-    double min_value, max_value, Z;
+    double min_value, max_value, z;
     minMaxLoc(output, &min_value, &max_value);
-    Z = 1/max_value * 255;
-    output = output * Z; // Normalize
+    z = 1 / max_value * 255;
+    output = output * z; // Normalize
     output.convertTo(output, CV_8U);
-}
-
-void remove_pixels(Mat& image, Mat &output, int* seam) {
-    for (int r = 0; r < image.rows; r++ ) {
-        for (int c = 0; c < image.cols; c++){
-            if (c >= seam[r])
-                output.at<Vec3b>(r, c) = image.at<Vec3b>(r, c+1);
-            else
-                output.at<Vec3b>(r, c) = image.at<Vec3b>(r, c);
-        }
-    }
 }
 
 /**
@@ -127,78 +124,151 @@ int* find_min_cut(Mat &energy_map){
  * @return An array of integers (which column to cut) representing a seam.
  */
 int* find_seam_of_frame(Mat &frame) {
-
-    //TODO: Might have to make it Mat& ^^^^
-
-    // Make it gray scale first
     Mat gray_frame;
     cvtColor(frame, gray_frame, COLOR_BGR2GRAY);
-
-    // Run through energy function
     Mat energy_map;
     energy_function(gray_frame, energy_map);
+    return find_min_cut(energy_map);
+}
+
+/**
+ * Removes a {seam} from a {frame}.
+ *
+ * @param frame - The frame to remove the seam from.
+ * @param seam - The seam to remove from the frame.
+ */
+void remove_seam(Mat &frame, const int* seam) {
+    int height = frame.rows;
+    int width = frame.cols;
+    Mat output(height, width-1, CV_8UC3);
+    for (int r = 0; r < height; r++) {
+        for (int c = 0; c < width; c++){
+            if (c >= seam[r]) {
+                output.at<Vec3b>(r, c) = frame.at<Vec3b>(r, c + 1);
+            } else {
+                output.at<Vec3b>(r, c) = frame.at<Vec3b>(r, c);
+            }
+        }
+    }
+
+    frame = output;
+}
+
+//----------------------------------//
+// Seam comparison helper functions //
+//----------------------------------//
+
+/**
+ * Calculates the difference between two seams.
+ *
+ * @param a - The first seam.
+ * @param b - The second seam.
+ * @param size - The size (height) of the seam.
+ * @return The difference (magnitude) of the seam.
+ */
+float calc_seam_diff(int* a, int* b, int size) {
+    float sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += sqrt(pow(a[i] - b[i], 2));
+    }
+
+    return sum;
+}
+
+/**
+ * Finds the index of the seam inside {seams} closest to {ref}.
+ *
+ * @param ref - The reference seam.
+ * @param seam_size - The size of the seam.
+ * @param seams - An array of seams.
+ * @param arr_size - The size of the array of seams.
+ * @return The index of the closest seam.
+ */
+int identify_closest_seam(int* ref, int seam_size, int** arr, int arr_size) {
+    int best_seam = 0;
+    float min_delta = calc_seam_diff(arr[0], ref, seam_size);
+    for (int i = 1; i < arr_size; i++) {
+        float delta = calc_seam_diff(arr[i], ref, seam_size);
+        if (delta < min_delta) {
+            best_seam = i;
+            min_delta = delta;
+        }
+    }
+
+    return best_seam;
+}
+
+//-------------------------------------------//
+// Incremental seam-carving helper functions //
+//-------------------------------------------//
+
+int* find_incremental_seam(Mat &frame, const int* prev_seam) {
+
+    // Create an energy map.
+    Mat gray_frame;
+    cvtColor(frame, gray_frame, COLOR_BGR2GRAY);
+    Mat energy_map;
+    energy_function(gray_frame, energy_map);
+
+    // Fill all cells other than +/- 1 pixels around prev_seam with infinity.
+    for (int r = 0; r < energy_map.rows; r++) {
+
+        // Find the "no-touch zone"
+        int center_col = prev_seam[r];
+        int left_bound = max(center_col-1, 0);
+        int right_bound = min(center_col+1, energy_map.cols);
+
+        // Cross out columns before "no-touch"
+        for (int c = 0; c < left_bound; c++) {
+            energy_map.at<uchar>(r, c) = (uchar)2147483647;
+        }
+
+        // Cross out columns after "no-touch"
+        for (int c = right_bound + 1; c < energy_map.cols; c++) {
+            energy_map.at<uchar>(r, c) = (uchar)2147483647;
+        }
+    }
 
     return find_min_cut(energy_map);
 }
 
-
+//-----------------------------//
+// Logistical helper functions //
+//-----------------------------//
 
 /**
- * Seam-carves the input video to fit the target depth.
- *
- * @param vid - The video to be seam-carved.
- * @param w - The width of the video.
- * @param h - The height of the video.
- * @param d - The depth (length in time) of the video.
- * @param target_d - The new depth (length in time) of the video.
+ * Parses command line argument from main()
+ * and returns corresponding VideoCapture object.
+ * @param argc - Passed in from main().
+ * @param argv - Passed in from main().
+ * @return The VideoCapture object derived from the command line argument.
  */
-void seam_carve_video(Mat* vid, int w, int h, int d, int target_d) {
-    assert(d >= target_d);
-
-    // Arrays to store seam information
-    int* avg_seam = new int[h];
-    int** all_seams = new int*[d];
-    for(int i = 0; i < d; ++i) {
-        all_seams[i] = new int[h];
+VideoCapture parse_input(int argc, char** argv) {
+    if (argc < 2) {
+        cerr << "Usage: ./seamCarving <VIDEO_FILE>" << endl;
+        exit(1);
     }
 
-    // Repeat until video is shortened to target_d.
-    for (int curr_d = d; curr_d > target_d; curr_d--) {
-
-        // Go through each frame finding seams.
-        for (int frame = 0; frame < w; frame++) {
-            int* min_seam = find_seam_of_frame(vid[frame]);
-
-            // Sum up the seams to calculate average later.
-            for (int row = 0; row < h; row++) {
-                avg_seam[row] += min_seam[row];
-            }
-        }
-
-        // Find the average value of cols that has seams.
-        for (int row = 0; row < h; row++) {
-            avg_seam[row] = avg_seam[row] / w;
-        }
-
-        // Identify one frame with a seam closest to the average seam.
-        int radix_frame = identify_closest_seam(all_seams, avg_seam);
-
-        // Remove seams from frames after the radix
-        int* prev_seam = all_seams[radix_frame];
-        for (int frame = radix_frame; frame < w; frame++) {
-            remove_seam(vid[frame], prev_seam);
-        }
-
-        // Remove seams from frames before the radix
-        prev_seam = all_seams[radix_frame];
-        for (int frame = radix_frame; frame > -1; frame--) {
-            if (frame == radix_frame) continue;
-            remove_seam(vid[frame], prev_seam);
-        }
+    VideoCapture cap(argv[1]);
+    if(!cap.isOpened()) {
+        cerr << "Error opening video stream or file" << endl;
+        exit(-1);
     }
 
-    delete[] avg_seam;
-    delete[] all_seams;
+    return cap;
+}
+
+/**
+ * Given a {start_time}, calculates and prints the split time.
+ *
+ * @param start_time - The start time of the split.
+ * @param header - A message explaining the purpose of the split time.
+ * @return The new start time of the next split.
+ */
+time_t take_split_time(time_t start_time, const String &header) {
+    double duration = (clock() - start_time) / (double)CLOCKS_PER_SEC;
+    cout << header << ": " << duration << endl;
+    return clock();
 }
 
 //--------------------------------------//
@@ -290,42 +360,72 @@ void export_video(Mat* vid, int fps, int w, int h, int d, const String &t) {
 }
 
 //-----------------------------//
-// Logistical helper functions //
+// Main seam-carving algorithm //
 //-----------------------------//
 
 /**
- * Parses command line argument from main()
- * and returns corresponding VideoCapture object.
- * @param argc - Passed in from main().
- * @param argv - Passed in from main().
- * @return The VideoCapture object derived from the command line argument.
- */
-VideoCapture parse_input(int argc, char** argv) {
-    if (argc < 2) {
-        cerr << "Usage: ./seamCarving <VIDEO_FILE>" << endl;
-        exit(1);
-    }
-
-    VideoCapture cap(argv[1]);
-    if(!cap.isOpened()) {
-        cerr << "Error opening video stream or file" << endl;
-        exit(-1);
-    }
-
-    return cap;
-}
-
-/**
- * Given a {start_time}, calculates and prints the split time.
+ * Seam-carves the input video to fit the target depth.
  *
- * @param start_time - The start time of the split.
- * @param header - A message explaining the purpose of the split time.
- * @return The new start time of the next split.
+ * @param vid - The video to be seam-carved.
+ * @param w - The width of the video.
+ * @param h - The height of the video.
+ * @param d - The depth (length in time) of the video.
+ * @param target_d - The new depth (length in time) of the video.
  */
-time_t take_split_time(time_t start_time, const String &header) {
-    double duration = (clock() - start_time) / (double)CLOCKS_PER_SEC;
-    cout << header << ": " << duration << endl;
-    return clock();
+void seam_carve_video(Mat* vid, int w, int h, int d, int target_d) {
+    assert(d >= target_d);
+
+    // Arrays to store seam information
+    int* avg_seam = new int[h];
+    int** all_seams = new int*[w];
+    for(int i = 0; i < w; ++i) {
+        all_seams[i] = new int[h];
+    }
+
+    // Repeat until video is shortened to target_d.
+    for (int curr_d = d; curr_d > target_d; curr_d--) {
+
+        // Go through each frame finding seams.
+        for (int frame = 0; frame < w; frame++) {
+            int* min_seam = find_seam_of_frame(vid[frame]);
+
+            // Sum up the seams to calculate average later.
+            for (int row = 0; row < h; row++) {
+                avg_seam[row] += min_seam[row];
+            }
+        }
+
+        // Find the average value of cols that has seams.
+        for (int row = 0; row < h; row++) {
+            avg_seam[row] = avg_seam[row] / w;
+        }
+
+        // Identify one frame with a seam closest to the average seam.
+        int radix_frame = identify_closest_seam(avg_seam, h, all_seams, w);
+
+        // Remove the seam from the radix.
+        int* cut_seam = all_seams[radix_frame];
+        remove_seam(vid[radix_frame], cut_seam);
+
+        // Remove seams from frames before the radix
+        Mat frame;
+        for (int i = 0; i < radix_frame; i++) {
+            frame = vid[i];
+            cut_seam = find_incremental_seam(frame, cut_seam);
+            remove_seam(frame, cut_seam);
+        }
+
+        // Remove seams from frames after the radix
+        cut_seam = all_seams[radix_frame];
+        for (int i = radix_frame; i < w; i++) {
+            frame = vid[i];
+            cut_seam = find_incremental_seam(frame, cut_seam);
+            remove_seam(frame, cut_seam);
+        }
+    }
+
+    delete[] avg_seam;
+    delete[] all_seams;
 }
 
 //---------------//
@@ -348,6 +448,8 @@ int main(int argc, char** argv) {
     int height = ceil(cap.get(CAP_PROP_FRAME_HEIGHT));
     int depth = ceil(cap.get(CAP_PROP_FRAME_COUNT));
     int target_depth = depth * 2 / 3;
+
+    cout << width << " " << height << " " << depth << endl;
 
     // CP: Finished opening connection to video, start rotating.
     start_time = take_split_time(start_time, "Opened video capture");
